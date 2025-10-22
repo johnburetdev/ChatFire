55cf079822d5ed05c6417762a94847c6bb98aa2b
johnburetdev <johnburet2000@gmail.com>
2025-10-22 12:23:42 -0400

    Proteccion de rutas implementada

--- src/layouts/admin.layout.tsx
+++ src/layouts/admin.layout.tsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { useSigninCheck } from "reactfire";
 
 const AdminLayout = () => {
+  const { status, data: signInCheckResult, hasEmitted } = useSigninCheck();
+
+  //Verificamos si el user esta logeado
+  if (status === "loading" || !hasEmitted) {
+    return <div>Loading...</div>;
+  }
+
+  //Redirigimos el usuario sin credenciales al logiNpage
+  if (!signInCheckResult.signedIn) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
   return (
-    <div>
+    <>
       <Outlet />
-    </div>
+    </>
   );
 };
 
--- src/layouts/auth.layout.tsx
+++ src/layouts/auth.layout.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { useSigninCheck } from "reactfire";
 
 const AuthLayout = () => {
+  const { status, data: signInCheckResult, hasEmitted } = useSigninCheck();
+
+  //Verificamos si el user esta logeado
+  if (status === "loading" || !hasEmitted) {
+    return <div>Loading...</div>;
+  }
+
+  //Dejamos que el usuario logeado tenga acceso, y mo tenga que volver a ver la pantalla de login
+  if (status === "success" && signInCheckResult.signedIn) {
+    return <Navigate to="/admin" replace />;
+  }
   return (
     <div>
       <Outlet />
